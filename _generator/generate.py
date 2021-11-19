a070ecf90954759fc58f7de0c99f4285bfc74c93
Martin Lukasik <martin@zipoapps.com>
2021-11-19 12:24:04 +0100

    Update generate.py

--- _generator/generate.py
+++ _generator/generate.py
@@ -8,14 +8,14 @@ parser.add_argument("--source", nargs=1, required=True)
 parser.add_argument("--ref", nargs=1)
 args = parser.parse_args()
 
+
 with open(args.source[0], "r") as csvfile:
     rows = csv.DictReader(csvfile)
     for row in rows:
         print(f'Triggering workflow for "{row["fullname"]}"')
         try:
-            print(row)
             subprocess.check_output([
-                "gh", "workflow", "run", "generate-app-pages.yml",
+                "gh", "workflow", "run", "generate.yml",
                 "--ref", args.ref[0] if args.ref is not None else "master",
                 "-f", "slug=" + row["slug"],
                 "-f", "fullname=" + row["fullname"],
@@ -26,5 +26,7 @@ with open(args.source[0], "r") as csvfile:
                 "-f", "package=" + row["package"],
             ])
         except subprocess.CalledProcessError as e:
-            print(f'Failed to triggering workflow for "{row["fullname"]}"')
+            print(
+                f'Failed to triggering workflow for "{row["fullname"]}" with the following inputs:')
+            print(row)
             input("Continue?")
